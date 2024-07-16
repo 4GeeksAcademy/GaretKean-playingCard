719f1cc320bb9a8fbc588b1f6339505c98e7a134
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-07-16 00:32:43 +0000

    unfinished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//array for suits & numbers
+const suits = [
+  ["♦", "diamonds"],
+  ["♥", "hearts"],
+  ["♠", "spades"],
+  ["♣", "clubs"]
+];
+// array for cardNumbers
+const cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const cardNumberIndex = Math.floor(Math.random() * cardNumber.length);
+  const randomCardNumber = cardNumber[cardNumberIndex];
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuit = suits[suitIndex];
+
+  // card top left
+  const topLeftSuitDivElement = document.querySelector(".top-Left-Suit");
+  if (!topLeftSuitDivElement) {
+    console.log("No top left suit element found");
+    return;
+  }
+  const randomSuitIcon = randomSuit[0];
+  const randomSuitClassName = randomSuit[1];
+  topLeftSuitDivElement.innerText = randomSuit;
+  topLeftSuitDivElement.classList.add(randomSuitClassName);
+
+  // Card Middle
+  const cardMiddleDivElement = document.querySelector(".card-middle");
+  if (!topLeftSuitDivElement) {
+    console.log("No middle element found");
+    return;
+  }
 };
