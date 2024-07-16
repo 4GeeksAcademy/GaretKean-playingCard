2a2783b9ff0fc9c52110a35f1fe574b07df328c2
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-07-16 16:51:28 +0000

    add width input and button

--- src/app.js
+++ src/app.js
@@ -11,36 +11,46 @@ const suits = [
 // array for cardNumbers
 const cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
-window.onload = function() {
-  // const cardNumberIndex = Math.floor(Math.random() * cardNumber.length);
-  // const randomCardNumber = cardNumber[cardNumberIndex];
-  // const suitIndex = Math.floor(Math.random() * suits.length);
-  // const randomSuit = suits[suitIndex][0];
-  // const suitIcon = suits[suitIndex][1];
-  // // card top left
-  // const topLeftSuitDivElement = document.querySelector(".top-Left-Suit");
-  // if (!topLeftSuitDivElement) {
-  //   console.log("No top left suit element found");
-  //   return;
-  // }
-  // const randomSuitIcon = randomSuit[0];
-  // const randomSuitClassName = randomSuit[1];
-  // topLeftSuitDivElement.innerText = randomSuit;
-  // topLeftSuitDivElement.classList.add(randomSuitClassName);
-
-  // // Card Middle
-  // const cardMiddleDivElement = document.querySelector(".card-middle");
-  // if (!topLeftSuitDivElement) {
-  //   console.log("No middle element found");
-  //   return;
-  // }
-
-  const suitIndex = Math.ceil(Math.random() * (suits.length - 1));
-  const suitIcon = suits[suitIndex][0];
+function generateRandomCard() {
+  //Try to generate variables in one place
+  const cardNumberIndex = Math.floor(Math.random() * cardNumber.length);
+  const randomCardNumber = cardNumber[cardNumberIndex];
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuit = suits[suitIndex][0];
   const suitClassName = suits[suitIndex][1];
 
-  topLeftRandomSuit = document.querySelector(".top-Left-Suit");
-  topLeftRandomSuit.innerText = suitIcon;
+  // card top left
+  const topLeftSuitDivElement = document.querySelector(".top-Left-Suit");
+  if (!topLeftSuitDivElement) {
+    console.log("No top left suit element found");
+    return;
+  }
+  topLeftSuitDivElement.innerText = randomSuit;
+  topLeftSuitDivElement.classList.add(suitClassName);
 
-  const faceNumberIndex = Math.ceil(Math.random() * (faceNumber.length - 1));
+  // card middle
+
+  const cardMiddleDivElement = document.querySelector(".card-middle");
+  if (!cardMiddleDivElement) {
+    console.log("No middle element found");
+    return;
+  }
+  cardMiddleDivElement.innerText = randomCardNumber;
+  cardMiddleDivElement.classList.add(suitClassName);
+
+  // card bottom right
+
+  const bottomRightSuitDivElement = document.querySelector(
+    ".bottom-right-suit"
+  );
+  if (!bottomRightSuitDivElement) {
+    console.log("No bottom right suit element found");
+    return;
+  }
+  bottomRightSuitDivElement.innerText = randomSuit;
+  bottomRightSuitDivElement.classList.add(suitClassName);
+}
+window.onload = function() {
+  generateRandomCard();
+  setInterval(generateRandomCard, 8000);
 };
