da8e98b5ab051ba106d25c1fc6e6526be55795e8
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-07-16 01:04:27 +0000

    unfinished

--- src/app.js
+++ src/app.js
@@ -12,26 +12,35 @@ const suits = [
 const cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
 window.onload = function() {
-  const cardNumberIndex = Math.floor(Math.random() * cardNumber.length);
-  const randomCardNumber = cardNumber[cardNumberIndex];
-  const suitIndex = Math.floor(Math.random() * suits.length);
-  const randomSuit = suits[suitIndex];
+  // const cardNumberIndex = Math.floor(Math.random() * cardNumber.length);
+  // const randomCardNumber = cardNumber[cardNumberIndex];
+  // const suitIndex = Math.floor(Math.random() * suits.length);
+  // const randomSuit = suits[suitIndex][0];
+  // const suitIcon = suits[suitIndex][1];
+  // // card top left
+  // const topLeftSuitDivElement = document.querySelector(".top-Left-Suit");
+  // if (!topLeftSuitDivElement) {
+  //   console.log("No top left suit element found");
+  //   return;
+  // }
+  // const randomSuitIcon = randomSuit[0];
+  // const randomSuitClassName = randomSuit[1];
+  // topLeftSuitDivElement.innerText = randomSuit;
+  // topLeftSuitDivElement.classList.add(randomSuitClassName);
 
-  // card top left
-  const topLeftSuitDivElement = document.querySelector(".top-Left-Suit");
-  if (!topLeftSuitDivElement) {
-    console.log("No top left suit element found");
-    return;
-  }
-  const randomSuitIcon = randomSuit[0];
-  const randomSuitClassName = randomSuit[1];
-  topLeftSuitDivElement.innerText = randomSuit;
-  topLeftSuitDivElement.classList.add(randomSuitClassName);
+  // // Card Middle
+  // const cardMiddleDivElement = document.querySelector(".card-middle");
+  // if (!topLeftSuitDivElement) {
+  //   console.log("No middle element found");
+  //   return;
+  // }
 
-  // Card Middle
-  const cardMiddleDivElement = document.querySelector(".card-middle");
-  if (!topLeftSuitDivElement) {
-    console.log("No middle element found");
-    return;
-  }
+  const suitIndex = Math.ceil(Math.random() * (suits.length - 1));
+  const suitIcon = suits[suitIndex][0];
+  const suitClassName = suits[suitIndex][1];
+
+  topLeftRandomSuit = document.querySelector(".top-Left-Suit");
+  topLeftRandomSuit.innerText = suitIcon;
+
+  const faceNumberIndex = Math.ceil(Math.random() * (faceNumber.length - 1));
 };
